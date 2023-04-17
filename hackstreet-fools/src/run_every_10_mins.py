e13a84f2d83f06925b45589eadad971b1f54c768
AKSHAT AGARWAL <57329437+AKSHAT564564@users.noreply.github.com>
2023-04-17 14:00:03 +0530

    10 mins run config

---
+++ hackstreet-fools/src/run_every_10_mins.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+def main():
+    # Replace 'your_script.py' with the name of the script you want to run
+    os.system('python3 main.py')
+
+if __name__ == '__main__':
+    while True:
+        main()
+        # Sleep for 10 minutes (600 seconds)
+        time.sleep(600)
